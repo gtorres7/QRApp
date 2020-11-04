44e8e09321c9e8beffb5d6f35756ec885888a8b7
gtorres7 <gtorres6@ucol.mx>
2020-11-04 00:59:59 -0600

    splash screen and navigation

--- qr_app/lib/main.dart
+++ qr_app/lib/main.dart
@@ -1,20 +1,40 @@
 import'package:flutter/material.dart';
 
 import 'pages/homepage.dart';
+import 'pages/scroll_page.dart';
 
-void main() {
- runApp(MaterialApp(
- home: MyApp(),
- debugShowCheckedModeBanner: false,
- ));
+// void main() {
+//  runApp(MaterialApp(
+//  home: MyApp(),
+//  debugShowCheckedModeBanner: false,
+//  ));
+// }
+// class MyApp extends StatefulWidget {
+//  @override
+//  _MyAppState createState() => _MyAppState();
+// }
+// class _MyAppState extends State<MyApp> {
+//  @override
+//  Widget build(BuildContext context) {
+//  return HomePage();
+//  }
+// }
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'scroll',
+      routes: {
+        
+        'scroll' : (BuildContext context) =>ScrollPage(),
+        "home"   :(BuildContext context) =>HomePage(),
+      },
+    );
+  }
 }
-class MyApp extends StatefulWidget {
- @override
- _MyAppState createState() => _MyAppState();
-}
-class _MyAppState extends State<MyApp> {
- @override
- Widget build(BuildContext context) {
- return HomePage();
- }
-}
+
--- qr_app/lib/pages/homepage.dart
+++ qr_app/lib/pages/homepage.dart
@@ -3,58 +3,57 @@ import 'package:flutter/material.dart';
 import 'generate.dart';
 import 'scan.dart';
 
-
-
 class HomePage extends StatefulWidget {
- @override
- _HomePageState createState() => _HomePageState();
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
- @override
- Widget build(BuildContext context) {
- return Scaffold(
- appBar: AppBar(
- title: Text("Inicio"),
- centerTitle: true,
- ),
- body: Container(
- height: MediaQuery.of(context).size.height,
- width: MediaQuery.of(context).size.width,
- padding: EdgeInsets.all(20.0),
- child: Column(
- mainAxisAlignment: MainAxisAlignment.center,
- crossAxisAlignment: CrossAxisAlignment.stretch,
- children: <Widget>[
- Image(image:
-NetworkImage("https://www.eluniversal.com.mx/sites/default/files/2020/08/26/pikachu_pokemon_ash.jpg")),
- flatButton("Escanear QR", ScanPage()),
- SizedBox(height: 20.0,),
- flatButton("Generar QR",
-GeneratePage()),
- ],
- ),
- ),
- );
- }
- Widget flatButton(String text, Widget widget) {
- return FlatButton(
- padding: EdgeInsets.all(15.0),
- onPressed: () async {
- Navigator.of(context)
- .push(MaterialPageRoute(builder:
-(context) => widget));
- },
- child: Text(
- text,
- style: TextStyle(color:
-Colors.blue,fontWeight: FontWeight.bold),
- ),
- shape: RoundedRectangleBorder(
- side: BorderSide(color: Colors.blue,width:
-3.0),
- borderRadius:
-BorderRadius.circular(20.0)),
- );
- }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inicio"),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image(
+                image: NetworkImage(
+                    "https://www.eluniversal.com.mx/sites/default/files/2020/08/26/pikachu_pokemon_ash.jpg")),
+            SizedBox( height: 20.0),
+            customButton("Escanear QR", ScanPage()),
+            SizedBox(
+              height: 20.0,
+            ),
+            customButton("Generar QR", GeneratePage()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customButton(String text, Widget widget) {
+    return RaisedButton(
+      color: Colors.blue,
+      padding: EdgeInsets.all(15.0),
+      onPressed: () async {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => widget));
+      },
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      shape: RoundedRectangleBorder(
+          //side: BorderSide(color: Colors.blue, width: 3.0),
+          borderRadius: BorderRadius.circular(20.0)),
+    );
+  }
 }
